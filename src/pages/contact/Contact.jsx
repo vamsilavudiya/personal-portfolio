112a7e6676a5ef9e35d7fd122684d389711722cd
vamsilavudiya <vamsilavadiyaa@gmail.com>
2023-05-11 09:23:31 +0530

    links fo rcall sms mail

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,7 +3,11 @@ import './contact.css';
 
 function Contact() {
   return (
-    <div id='contact' className='contact'>Contact</div>
+    <div id='contact' className='contact'>
+      <a href = "mailto:vamsilavadiyaa@gmail.com"> Send Me a Mail</a>
+      <a href="sms:+919959553143"> Send Me a SMS</a>
+      <a href = "callto:+919959553143"> Call Me</a>
+    </div>
   )
 }
 
