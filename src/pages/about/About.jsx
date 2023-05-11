0ad0c8792ecdaf06e484c2e90b1636128602fffd
vamsilavudiya <vamsilavadiyaa@gmail.com>
2023-05-11 11:04:20 +0530

    added about and contact

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './about.css';
+import ImageForAbout from '../../assets/web.svg'
 
 function About() {
   return (
-    <div id='about' className='about'>About</div>
+    <div id='about' className='about'>
+      <div className="left">
+        <img src={ImageForAbout} alt="" />
+      </div>
+      <div className="right">
+        <div className="container">
+          <h2>About Me</h2>
+          <h1>I'm Vamsi Lavudiya</h1>
+          <div>
+          <div className="right-left">
+            <span>Date of Birth</span>
+            <span>Nick Name</span>
+            <span>Profession</span>
+            <span>Address</span>
+          </div>
+          <div className="right-right">
+            <span>25 Aug 2000</span>
+            <span>Vamsi</span>
+            <span>Web Developer</span>
+            <span>Jaggayyapeta, <br /> andhra pradesh - 521190</span>
+          </div>
+        </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
 
 function Contact() {
   return (
-    <div id='contact' className='contact'>
-      <a href = "mailto:vamsilavadiyaa@gmail.com"> Send Me a Mail</a>
-      <a href="sms:+919959553143"> Send Me a SMS</a>
-      <a href = "tel:+919959553143"> Call Me</a>
+    <div id="contact" className="contact">
+      <div className="container">
+        <h1>Contact Me</h1>
+        <div className="inputs">
+          <span>Name</span>
+          <input type="text" placeholder="Enter Your Name" />
+          <span>Email</span>
+          <input type="text" placeholder="Enter Your Email" />
+          <span>Mobile Number</span>
+          <input type="text" placeholder="Enter Your Number" />
+          <span>Message</span>
+          <textarea placeholder="Enter Your text here" />
+          <button type="button">Send Message</button>
+          <div>
+            <a href="mailto:vamsilavadiyaa@gmail.com"> Send Me a Mail</a>
+            <a href="sms:+919959553143"> Send Me a SMS</a>
+            <a href="tel:+919959553143"> Call Me</a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
