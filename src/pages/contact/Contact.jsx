53cfa54a147a004240e0eaf0080137559400381f
vamsilavudiya <vamsilavadiyaa@gmail.com>
2023-05-11 13:21:32 +0530

    messagge sending thing

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -8,12 +8,13 @@ function Contact() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_KEY, form.current,process.env.PUBLIC_KEY )
+    emailjs.sendForm("service_uq9xil9", "template_3z47gpj", form.current,"G4AlsdIDBljDKid5y" )
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+      alert("succesfully submitted Your Messagge")
   };
   return (
     <div id="contact" className="contact">
@@ -21,13 +22,13 @@ function Contact() {
         <h1>Contact Me</h1>
         <form ref={form} onSubmit={sendEmail} className="inputs">
           <span>Name</span>
-          <input type="text" name='user_name' placeholder="Enter Your Name" />
+          <input type="text" name='user_name' placeholder="Enter Your Name" required/>
           <span>Email</span>
-          <input type="text" name='user_email' placeholder="Enter Your Email" />
+          <input type="text" name='user_email' placeholder="Enter Your Email" required/>
           <span>Mobile Number</span>
-          <input type="text" name='user_number' placeholder="Enter Your Number" />
+          <input type="text" name='user_number' placeholder="Enter Your Number" required/>
           <span>Message</span>
-          <textarea name='message' placeholder="Enter Your text here" />
+          <textarea name='message' placeholder="Enter Your text here" required/>
           <button type="submit">Send Message</button>
           <div>
             <a href="mailto:vamsilavadiyaa@gmail.com"> Send Me a Mail</a>
